10f424fc64a45bf597e9b1ec78cc1fbfc546ceb9
Marko Banusic <mbanusic@gmail.com>
2017-11-26 15:05:46 +0100

    Remove migrations

--- src/MessagingServiceProvider.php
+++ src/MessagingServiceProvider.php
@@ -17,8 +17,6 @@ class MessagingServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-	    $this->loadMigrationsFrom(__DIR__ . '/database');
-
 	    $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
 	    $this->loadRoutesFrom(__DIR__ . '/routes/api.php');
 
